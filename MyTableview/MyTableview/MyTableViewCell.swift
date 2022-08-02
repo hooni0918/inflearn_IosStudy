5f81e3e2d8d759bf565d4f9b72f7ff36b1ae915f
YIZIHN <109647045+hooni0918@users.noreply.github.com>
2022-08-02 23:41:10 +0900

    Add files via upload
    Tableview

---
+++ MyTableview/MyTableview/MyTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyTableViewCell.swift
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var idolImageView: UIImageView!
+    @IBOutlet weak var lableName: UILabel!
+    @IBOutlet weak var lableArt: UILabel!
+    
+}
---
+++ MyTableview/MyTableview/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  MyTableview
+//
+//  Created by 이지훈 on 2022/08/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    let name:[String] = [
+        "카리나","태연","아이유","윤아","수지"
+    ]
+    let arts:[String] = [
+        "블랙맘바,파워업","호텔델루다,사계","좋은날,스물둘","효리네민박,바람이불면","건축학개론,페이스러브"
+    ]
+    let images:[String] = [
+        "image1.png","image2.png","image3.png","image4.png","image5.png"
+    ]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+}
+
