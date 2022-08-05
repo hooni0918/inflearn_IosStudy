6f266b38d8de840e95e9f1199aab34b7f58ded08
YIZIHN <109647045+hooni0918@users.noreply.github.com>
2022-08-05 20:25:24 +0900

    Autolayout
    Autolayout

---
+++ 0910Autolayout~Tablelist/0910Autolayout~Tablelist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  0910Autolayout~Tablelist
+//
+//  Created by 이지훈 on 2022/08/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
