727d27c0466f8ac407b48335dfcf6ba76846b159
YIZIHN <109647045+hooni0918@users.noreply.github.com>
2022-08-15 23:28:32 +0900

    Add files via upload
    btnview

---
+++ Ex07_Button/Ex07_Button/Ex07_ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ex07_ButtonApp.swift
+//  Ex07_Button
+//
+//  Created by imac_kkn on 2021/10/15.
+//
+
+import SwiftUI
+
+@main
+struct Ex07_ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
