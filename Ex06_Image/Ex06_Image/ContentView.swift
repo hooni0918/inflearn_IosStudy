567fb98307d8d407e0ad1994a25616c244a2ae68
YIZIHN <109647045+hooni0918@users.noreply.github.com>
2022-08-15 23:28:07 +0900

    Add files via upload
    imageview

---
+++ Ex06_Image/Ex06_Image/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  Ex06_Image
+//
+//  Created by imac_kkn on 2021/10/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            //가로든 세로든 이미지가 큰 쪽이 테두리에 닿으면 크기가 정해짐.
+            Image("waterfall")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Divider()
+            
+            Image("waterfall2")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 250.0, height: 250.0, alignment: .top)
+            
+            Image(systemName: "play.circle")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(Color.red)
+            
+            Divider()
+            
+            Text("WaterFall")
+                .background( Image("waterfall")
+                                .resizable()
+                                .frame(width: 200, height: 200, alignment: .center) )
+                .frame(width: 200, height: 200, alignment: .center)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .shadow( radius: 10 )
+                .overlay(Circle().stroke(Color.red, lineWidth: 5))
+                
+            
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ex06_Image/Ex06_Image/Ex06_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ex06_ImageApp.swift
+//  Ex06_Image
+//
+//  Created by imac_kkn on 2021/10/14.
+//
+
+import SwiftUI
+
+@main
+struct Ex06_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
