5be0cfc207c3f56795404f7fb51b473c7ad25f2a
YIZIHN <109647045+hooni0918@users.noreply.github.com>
2022-08-15 23:27:40 +0900

    Add files via upload
    spacer

---
+++ Ex05_SpacerDivider/Ex05_SpacerDivider/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  Ex05_SpacerDivider
+//
+//  Created by imac_kkn on 2021/10/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            VStack {
+//                Spacer()
+                Color.yellow
+                    .frame(height: 100)
+                Spacer()
+                Color.red
+                    .frame(height: 100)
+//                Spacer()
+            }
+//            HStack() {
+//                Spacer()
+//                Color.yellow
+//                    .frame(width: 100)
+//                Spacer()
+//                Color.red
+//                    .frame(width: 100)
+//                Spacer()
+//            }
+//            VStack {
+//                Divider()
+//                Color.yellow
+//                    .frame(height: 100)
+//                Divider()
+//                Color.red
+//                    .frame(height: 100)
+//                Divider()
+//            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+            ContentView()
+        }
+    }
+}
---
+++ Ex05_SpacerDivider/Ex05_SpacerDivider/Ex05_SpacerDividerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ex05_SpacerDividerApp.swift
+//  Ex05_SpacerDivider
+//
+//  Created by imac_kkn on 2021/10/12.
+//
+
+import SwiftUI
+
+@main
+struct Ex05_SpacerDividerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
