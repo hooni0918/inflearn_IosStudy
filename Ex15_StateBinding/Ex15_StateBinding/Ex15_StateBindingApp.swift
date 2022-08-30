cc1c569592c026282f41c5f1d380414ffebbe4da
YIZIHN <109647045+hooni0918@users.noreply.github.com>
2022-08-30 20:16:44 +0900

    Add files via upload

---
+++ Ex15_StateBinding/Ex15_StateBinding/Ex15_StateBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ex15_StateBindingApp.swift
+//  Ex15_StateBinding
+//
+//  Created by imac_kkn on 2021/10/27.
+//
+
+import SwiftUI
+
+@main
+struct Ex15_StateBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
