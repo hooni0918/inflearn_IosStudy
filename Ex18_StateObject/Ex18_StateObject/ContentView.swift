b46ebe1076618cf77ceb96a9e599e04a31826fe2
YIZIHN <109647045+hooni0918@users.noreply.github.com>
2022-09-10 22:05:14 +0900

    Add files via upload

---
+++ Ex18_StateObject/Ex18_StateObject/ContentView.swift
@@ -0,0 +1,79 @@
+//
+//  ContentView.swift
+//  Ex18_StateObject
+//
+//  Created by imac_kkn on 2021/10/27.
+//
+
+import SwiftUI
+import Foundation
+
+// @Stagte @Binding :
+// Struct 사이의 값을 공유할때 사용한다.
+
+//@ObservedObject @ObservableObject
+// Class와 Struct 사이의 값을 공유(전달)할때 사용한다.
+//특징: 뷰의 라이플 사이클에 의존합니다. 뷰가 소멸할 때 값도 같이 초기화되어짐.
+
+//@StateObject 는 ObservableObject를 인스턴스화를 하고 observable object 내에
+//@Published 속성으로 선언된 프로퍼티의 값이 변할 때 뷰를 업데이트 합니다.
+// 특징 : 뷰의 라이플 사이클에 의존하지 않습니다. 뷰가 소멸할 때 값을 유지할 수 있다.
+// 버전 : SwiftUI 2(iOS14에서 추가된 기능)부터 사용가능합니다.
+
+class Counter: ObservableObject {
+    @Published var count: Int = 0
+    
+    init() {
+        
+    }
+    
+    func add() {
+        self.count += 1
+    }
+    
+}
+
+struct CounterView: View {
+    //@ObservedObject var counter: Counter //뷰가 새로 생성되면서, 가지고 있던 값이 초기화되어짐.
+    @StateObject var counter: Counter //뷰가 새로 생성되어도, 기존에 있던 값을 유지한다.
+    
+    var body: some View {
+        HStack {
+            Text("\(counter.count)")
+            Button {
+                self.counter.add()
+            } label: {
+                Text("Add")
+            }
+
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+    }
+    
+}
+
+struct ContentView: View {
+    @State private var isChanged: Bool = false
+    
+    var body: some View {
+        VStack {
+            CounterView(counter: Counter())
+                .background( isChanged ? Color.red : Color.green )
+                .padding()
+            Button {
+                self.isChanged.toggle()
+            } label: {
+                Text("Change")
+            }
+
+        }
+        .font(.largeTitle)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ex18_StateObject/Ex18_StateObject/Ex18_StateObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ex18_StateObjectApp.swift
+//  Ex18_StateObject
+//
+//  Created by imac_kkn on 2021/10/27.
+//
+
+import SwiftUI
+
+@main
+struct Ex18_StateObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
