29789a97b309b055da528a32f380f5ec0c477f2e
YIZIHN <109647045+hooni0918@users.noreply.github.com>
2022-09-15 00:26:36 +0900

    Add files via upload

---
+++ Ex22_List/Ex22_List/Ex22_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ex22_ListApp.swift
+//  Ex22_List
+//
+//  Created by imac_kkn on 2021/11/07.
+//
+
+import SwiftUI
+
+@main
+struct Ex22_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
