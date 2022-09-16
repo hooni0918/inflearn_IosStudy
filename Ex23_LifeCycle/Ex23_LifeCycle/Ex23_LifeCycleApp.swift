5ef5736fb65135f2d3ccf4fb21e37d373729ba29
YIZIHN <109647045+hooni0918@users.noreply.github.com>
2022-09-17 00:03:44 +0900

    Add files via upload

---
+++ Ex23_LifeCycle/Ex23_LifeCycle/Ex23_LifeCycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ex23_LifeCycleApp.swift
+//  Ex23_LifeCycle
+//
+//  Created by imac_kkn on 2021/11/07.
+//
+
+import SwiftUI
+
+@main
+struct Ex23_LifeCycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
